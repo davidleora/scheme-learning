ebeea3e653f85f859b463b6599e088dd3a7ce499
David LEORA <liuleora@gmail.com>
2024-07-02 15:16:02 +0900

    Updated Ext1 part

--- task/task2.scm
+++ task/task2.scm
@@ -5,11 +5,21 @@
   (read (open-input-file "./tokugawa")))
 
 ; Ext 1
-(define get-depth
-  (lambda (tree depth)
-    (cond
-      ((= depth 0) (list tree))
-      ((pair? tree) (append (get-depth (car tree) (- depth 1))
-                           (get-depth (cdr tree) depth)))
-      (else '()))
-    ))
+(define (get-depth tree depth)
+  (cond
+    ((null? tree) '())  ; Handle an empty tree
+    ((= depth 0)        ; We're at the target depth
+     (if (pair? tree)   ; If tree is a pair (i.e., non-leaf node)
+         (if (list? (car tree))  ; If car is a list, skip it at depth 1
+             '()
+             (list (car tree)))  ; Collect non-list car elements
+         '()))
+    ((list? tree)       ; If tree is a list and not at depth 1
+     (apply append (map (lambda (sub-tree)  ; Apply to each subtree
+                          (get-depth sub-tree (- depth 1)))
+                        tree)))
+    (else '())))  ; Return an empty list in other cases
+
+
+
+

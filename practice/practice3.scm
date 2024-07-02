04b16080c4bce09b2eb3ccdfd3123ce4cc594d11
David LEORA <liuleora@gmail.com>
2024-07-02 12:52:17 +0900

    Updated practice3.scm

--- practice/practice3.scm
+++ practice/practice3.scm
@@ -3,3 +3,11 @@
     (map (lambda (x) (* x n)) ls)
   )
 )
+
+; recursive method
+(define timess
+  (lambda (n ls)
+    (if (null? ls) '()
+       (cons (* n (car ls)) (timess n (cdr ls))))
+    )
+  )
